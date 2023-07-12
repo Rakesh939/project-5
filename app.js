79956747f54ed9d725a20fd0fb3020e8a3c48c0f
Rakesh939 <139116747+Rakesh939@users.noreply.github.com>
2023-07-12 18:29:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var image =document.getElementById("image");
+function bulb_on(){
+    image.src = "./glowing-yellow-light-bulb-vector-8566529.jpg";
+
+}
+function bulb_off(){
+    image.src = "./download.jpeg";
+}
